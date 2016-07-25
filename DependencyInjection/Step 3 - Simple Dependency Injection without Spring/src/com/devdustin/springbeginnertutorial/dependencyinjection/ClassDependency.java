76f1eac2a89c45b096904e27ea9e828edd96d9c9
Dustin Kendall <dustinkendall@gmail.com>
2016-07-25 17:02:32 -0600

    tweak example to show DI more clearly

--- DependencyInjection/Step 3 - Simple Dependency Injection without Spring/src/com/devdustin/springbeginnertutorial/dependencyinjection/ClassDependency.java
+++ DependencyInjection/Step 3 - Simple Dependency Injection without Spring/src/com/devdustin/springbeginnertutorial/dependencyinjection/ClassDependency.java
@@ -3,10 +3,6 @@ package com.devdustin.springbeginnertutorial.dependencyinjection;
 public class ClassDependency {
 	
 	private String injectionType;
-		
-	public void printInjectionType() {
-		System.out.println("running class dependency method...injection type is " + injectionType);
-	}
 	
 	public String getInjectionType() {
 		return injectionType;
--- DependencyInjection/Step 3 - Simple Dependency Injection without Spring/src/com/devdustin/springbeginnertutorial/dependencyinjection/ExampleRunner.java
+++ DependencyInjection/Step 3 - Simple Dependency Injection without Spring/src/com/devdustin/springbeginnertutorial/dependencyinjection/ExampleRunner.java
@@ -6,25 +6,25 @@ public class ExampleRunner {
 	
 	// constructor injection
 	public ExampleRunner(ClassDependency dependency) {
-		this.classDependency = dependency;
+		dependency.setInjectionType("constructor");
+		this.classDependency = dependency;				
 	}
 	
 	// setter injection
 	public void setClassDependency(ClassDependency dependency) {
-		this.classDependency = dependency;
+		dependency.setInjectionType("setter");
+		this.classDependency = dependency;	
 	}
 
 	public void run() {
 		System.out.println("running...");
-		
-		this.classDependency.printInjectionType();
+		System.out.println(String.format("Dependency injection that was used: %s", this.classDependency.getInjectionType()));
 	}
 
 	public static void main(String[] args) {
 		/* CONSTRUCTOR INJECTION */
 		ClassDependency constructorInjectionDependency = new ClassDependency();
-		constructorInjectionDependency.setInjectionType("from constructor");
-
+		
 		// constructor dependency injection happens here
 		ExampleRunner runner = new ExampleRunner(constructorInjectionDependency);
 		
